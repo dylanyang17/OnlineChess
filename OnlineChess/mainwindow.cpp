89021cc18bcb69545220cd2e022082889b5bd173
yyr <315629555@qq.com>
2019-08-26 22:26:12 +0800

    grid is finished.

--- OnlineChess/mainwindow.cpp
+++ OnlineChess/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPainter>
+#include <QDebug>
+#include <QColor>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -8,22 +10,72 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     gridSize=40;
+    tagSize=gridSize/8 ;
     col=row=8;
     leftUp = QPoint(50,50) ;
+    groundColor[0] = QColor(240,218,181) ;
+    groundColor[1] = QColor(185,131,99) ;
 }
 
 QPoint MainWindow::getPoint(int x, int y){
-    return QPoint((x-1)*gridSize, (y-1)*gridSize);//TODO
+    y = row+2-y;
+    return QPoint((x-1)*gridSize, (y-1)*gridSize);
+}
+
+int MainWindow::char2ind(QChar s){
+    return s.toLatin1()-'a'+1;
+}
+
+QChar MainWindow::ind2char(int a){
+    return QChar(a+'a'-1) ;
+}
+
+int MainWindow::getGroundType(int x, int y){
+    //返回地面颜色（黑色为1，白色为0）
+    return (x+y)%2^1 ;
 }
 
 void MainWindow::paintEvent(QPaintEvent *event){
     QPainter painter(this) ;
+    painter.setRenderHint(QPainter::Antialiasing, true); // 抗锯齿
     painter.translate(leftUp) ;
+
+    //网格及边线
     for(int i=1;i<=col;++i){
         for(int j=1;j<=row;++j){
-
+            QPoint p1=getPoint(i,j+1), p2=getPoint(i+1,j) ;
+            painter.setPen(Qt::NoPen);
+            painter.setBrush(QBrush(groundColor[getGroundType(i,j)], Qt::SolidPattern)) ;
+            painter.drawRect(QRect(p1,p2)) ;
         }
     }
+    painter.setPen(Qt::SolidLine);
+    for(int i=1;i<=col;++i) {
+        painter.setPen(Qt::black);
+        painter.drawLine(getPoint(i, 1), getPoint(i+1, 1)) ;
+        painter.drawLine(getPoint(i, row+1), getPoint(i+1, row+1)) ;
+        QPoint tmp = getPoint(i+1, 1) ;
+        painter.setPen(groundColor[getGroundType(i,1)^1]);
+        QFont font = painter.font();
+        font.setPointSize(tagSize) ;
+        painter.drawText(tmp.x()-tagSize-3, tmp.y()-3, ind2char(i)) ;
+    }
+    for(int j=1;j<=row;++j) {
+        painter.setPen(Qt::black);
+        painter.drawLine(getPoint(1, j), getPoint(1, j+1)) ;
+        painter.drawLine(getPoint(col+1, j), getPoint(col+1, j+1)) ;
+        QPoint tmp = getPoint(1, j+1) ;
+        painter.setPen(groundColor[getGroundType(1,j)^1]);
+        QFont font = painter.font();
+        font.setPointSize(tagSize) ;
+        painter.drawText(tmp.x()+3, tmp.y()+tagSize+6, QString::number(j)) ;
+    }
+    QPoint tmp = getPoint(col+1,1);
+    this->setMinimumSize(tmp.x()+110, tmp.y()+80);
+    this->setMaximumSize(tmp.x()+110, tmp.y()+80);
+
+
+
 }
 
 MainWindow::~MainWindow()
--- OnlineChess/mainwindow.h
+++ OnlineChess/mainwindow.h
@@ -18,10 +18,14 @@ public:
 
     void paintEvent(QPaintEvent *event);
     QPoint getPoint(int x, int y);
+    int char2ind(QChar s);
+    QChar ind2char(int a);
+    int getGroundType(int x, int y);
 private:
     Ui::MainWindow *ui;
-    int gridSize, col, row;
+    int gridSize, col, row, tagSize;
     QPoint leftUp ;
+    QColor groundColor[2];
 };
 
 #endif // MAINWINDOW_H
